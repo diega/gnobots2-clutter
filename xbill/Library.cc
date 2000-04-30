2312fe54d2a82cd981319fc28fac7b0622ab3b08
James Cape <jcape@jcinteractive.com>
2000-04-30 05:24:21 +0000

    Updated all remaining pixmaps to look better. Added debian.xpm, beos.xpm,
    2000-04-30  James Cape  <jcape@jcinteractive.com>
    
            * pixmaps/*.xpm: Updated all remaining pixmaps to look better.
            * pixmaps: Added debian.xpm, beos.xpm, turbolinux.xpm, suse.xpm, and
              caldera.xpm
    
            * cursors/*.xpm: Updated all cursors to match pixmaps.
            * cursors: Added debian.xpm, beos.xpm, turbolinux.xpm, suse.xpm, and
              caldera.xpm.
    
            * Library.cc, Library.h: Added new pixmaps so they are used by
              the game.
    
            * pixmaps/Makefile.in, cursors/Makefile.in: Added in new pixmaps and
              cursors so they are installed.

--- xbill/Library.cc
+++ xbill/Library.cc
@@ -3,7 +3,8 @@
 void Library::load_pix() {
 	int i;
         char *name[] = {"wingdows", "apple", "next", "sgi", "sun", "os2",
-		"bsd", "linux", "redhat", "hurd"};
+		"bsd", "linux", "redhat", "hurd", "debian", "beos",
+		"mandrake", "turbolinux", "suse", "caldera"};
 	for (i=0; i <= NUM_OS; i++) {
 		os[i].load(name[i]);
 		if (i) cursor[i].load(name[i], cursor[i].OWN_MASK);
