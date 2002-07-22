c8d76e2c0b118af8b7d5b8887a060a6df8d7d19d
Ross Burton <rburton@src.gnome.org>
2002-07-22 15:29:00 +0000

    Back out of an accidental commit. Will go back in when its tested...

--- gataxx/gataxx.c
+++ gataxx/gataxx.c
@@ -229,22 +229,16 @@ gboolean quit_game_cb(GtkWidget *widget, gpointer data)
     gint response;
 
     box = gtk_message_dialog_new (GTK_WINDOW (window),
-                                 GTK_DIALOG_MODAL,
+                                 GTK_DIALOG_DESTROY_WITH_PARENT,
                                  GTK_MESSAGE_QUESTION,
-                                 GTK_BUTTONS_NONE,
-                                 _("Really exit Gataxx?"));
-    gtk_dialog_add_buttons (GTK_DIALOG (box),
-            GTK_STOCK_CANCEL, GTK_RESPONSE_REJECT,
-            GTK_STOCK_QUIT, GTK_RESPONSE_ACCEPT,
-            NULL);
-
-    
-    gtk_dialog_set_default_response (GTK_DIALOG (box), GTK_RESPONSE_REJECT);
+                                 GTK_BUTTONS_YES_NO,
+                                 _("Do you really want to quit?"));
+    gtk_dialog_set_default_response (GTK_DIALOG (box), GTK_RESPONSE_YES);
 
     response = gtk_dialog_run (GTK_DIALOG (box));
     gtk_widget_destroy (box);
 
-    if (response == GTK_RESPONSE_REJECT)
+    if (response == GTK_RESPONSE_NO)
       return TRUE;
   }
 
