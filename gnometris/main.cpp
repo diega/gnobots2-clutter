4d5829592d79953ca9579ddfd02f8b0b06f17f99
Christian Persch <chpe@src.gnome.org>
2007-06-28 12:28:16 +0000

    Fix for games-sound API change
    svn path=/trunk/; revision=6469

--- gnometris/main.cpp
+++ gnometris/main.cpp
@@ -46,7 +46,7 @@ main(int argc, char *argv[])
 	GOptionContext *context = g_option_context_new (NULL);
 
 	g_option_context_add_main_entries (context, options, GETTEXT_PACKAGE);
-	g_option_context_add_group (context, games_sound_get_option_group ());
+        games_sound_add_option_group (context);
 
 	GnomeProgram *program = gnome_program_init ("gnometris", VERSION,
 						    LIBGNOMEUI_MODULE,
