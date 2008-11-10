f837913b70c52e79a699a2b8220dd00ca21c2dab
Christian Persch <chpe@src.gnome.org>
2008-11-10 14:52:53 +0000

    The window-state-event callback needs to return gboolean, not void. [2-24-candidate]
    svn path=/trunk/; revision=8307

--- blackjack/src/blackjack.cpp
+++ blackjack/src/blackjack.cpp
@@ -179,12 +179,13 @@ fullscreen_cb (GtkAction *action)
 }
 
 /* Just in case something else takes us to/from fullscreen. */
-static void 
+static gboolean
 window_state_cb (GtkWidget *widget, GdkEventWindowState *event)
 {
 	if (event->changed_mask & GDK_WINDOW_STATE_FULLSCREEN)
         	set_fullscreen_actions (event->new_window_state &
 					GDK_WINDOW_STATE_FULLSCREEN);
+	return FALSE;
 }
 
 
