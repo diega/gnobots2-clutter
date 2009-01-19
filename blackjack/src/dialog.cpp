diega/gnobots2-clutter

commit 539a2f68f396b1fbfba7d825ed320e8294f36fb2

Fix percentage format in translated string.
svn path=/trunk/; revision=8374

blackjack/src/dialog.cpp
@@ -49,7 +49,7 @@ get_insurance_choice (void)
         gboolean    choice = FALSE;
 
         message = _("Would you like insurance?");
-        secondary_message = _("Insurance is a side wager of 50% of the original wager"
+        secondary_message = _("Insurance is a side wager of 50%% of the original wager"
                               " that the dealer has a natural 21 (aka blackjack) that is"
                               " offered when the dealer's face up card is an ace. If the"
                               " dealer has a natural 21 then the player is paid double.");

commit c4adbf66c6172bdd9f63115fd26d849643812f41

Fix bug introduced in r8374: this isn't a c format, no need to double the %
svn path=/trunk/; revision=8597

blackjack/src/dialog.cpp
@@ -49,7 +49,8 @@ get_insurance_choice (void)
         gboolean    choice = FALSE;
 
         message = _("Would you like insurance?");
-        secondary_message = _("Insurance is a side wager of 50%% of the original wager"
+        /* xgettext:no-c-format */
+        secondary_message = _("Insurance is a side wager of 50% of the original wager"
                               " that the dealer has a natural 21 (aka blackjack) that is"
                               " offered when the dealer's face up card is an ace. If the"
                               " dealer has a natural 21 then the player is paid double.");